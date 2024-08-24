204b973673c58db8a59dfd205aae4281c1a72a41
saalfernandes <dudaf1691@gmail.com>
2024-08-24 16:55:12 -0300

    Add files via upload

---
+++ AULA-14 2.0/script.js
@@ -0,0 +1,11 @@
+//const frutas = ("banana, caju, abacaxi, figo, damasco, embaúba")
+//const numeros = (12, 3, 20, 7, 32, 1)
+//const pessoas = [
+    //{
+     //nome: 'Duda Fernandes',
+  //   idade: "17"
+//    }
+  //  ]
+
+//const texto = "banana, caju, abacaxi, figo, damasco, embaúba"
+//console.log(texto.split(""))
---
+++ AULA-14/script.js
@@ -0,0 +1,82 @@
+//Questão1:
+//let index = 1
+
+//while (index <= 10) {
+  //  console.log(index); index = index + 1
+//}
+
+//questão2:
+//let index = 10
+
+//while (index >= 0) {
+  //  console.log(index); index = index - 1
+//}
+
+//questão3:
+//let index = 1
+
+//while (index <= 20) {
+  //  console.log(index); index = index + 1
+//}
+
+//questão4:
+//let index = 1
+
+//while (index <= 20) {
+   // if(index % 2 !== 0) {
+ //       console.log(index); 
+//    }
+  //  index = index +1 
+//}
+
+//questão5:
+//let soma = 0
+
+//let index = 1
+
+//while (index <= 100) {
+  // soma += index
+  // index++
+//}
+//console.log(soma)
+
+//questão6:
+//const numero = +prompt("Digite um número: ")
+//let fatorial = 1
+
+//for(let i = numero; i >= 1; i--) {
+  // fatorial *= i
+//}
+//console.log(fatorial)
+
+//questão7:
+//const numeros = [12, 34, 45, 56, 67]
+
+//for (const numero of numeros) {
+ //   console.log(numeros)
+//}
+
+//questão8:
+//const  palavras  = prompt("Digite uma palavra: ")
+//const letras = palavras.split("")
+//let contador = 0
+
+//for (const letra of letras) {
+    //if (letra === "a"|| letra === "e"||letra === "i"||letra === "o"||letra === "u"//) {
+    //    contador++
+  //  }
+//}
+
+//console.log(contador)
+
+//questão9:
+//const palavras = prompt("Digite uma palavra: ")
+//const letras = palavras.split(""). reverse()
+//console.log(letras)
+//let palavraInversa = ""
+
+//for(const letra of letras) {
+//    palavraInversa += letra
+//}
+
+//console.log(letras)

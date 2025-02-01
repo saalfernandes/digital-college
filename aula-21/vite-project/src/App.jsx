e9fcf0170fba1cc36fdc81fd857a94b5d45bc53e
saalfernandes <dudaf1691@gmail.com>
2025-02-01 11:58:45 -0300

    Add files via upload

---
+++ aula-21/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./layouts/footer"
+import Header from "./layouts/Header"
+import Main from "./layouts/main"
+function App() {
+  return (
+    <>
+   <Header />
+   <Main />
+   <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ aula-21/vite-project/src/layouts/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header() {
+    return (
+        <header className="vh-10 bg-dark text-white">
+            <div className="d-flex justify-content-between p-3">
+            <div>
+                Logo
+            </div>
+            <div>
+                Menu
+                </div>
+            </div>
+        </header>
+       
+    )
+}
+
+export default Header;
